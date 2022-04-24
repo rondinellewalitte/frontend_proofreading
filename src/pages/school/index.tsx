acd864b80414e844b4f78b4788f063b219be5488
Rondinelle DRR <rondinellewalitte@gmail.com>
2022-04-24 10:25:06 -0300

    Segundo Commit

---
+++ src/pages/school/index.tsx
@@ -0,0 +1,39 @@
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack, Select } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function CreateSchool() {
+  return (
+    <Box>
+      <Header />
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">Inserir Escola</Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="school" label="Nome da Escola" />
+            </SimpleGrid>
+          </VStack>
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
